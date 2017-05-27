dc79f2b19f0078cc02478987418227bddbe7875c
Tim Doyle <coder.td@gmail.com>
2017-05-27 10:50:59 -0700

    Get Appointments Ajax

--- www/assets/js/app/cgi.js
+++ www/assets/js/app/cgi.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
 
 function getAppointments() {
     console.log("in get appointments, sending ajax");
+
     var response;
     var url = '/index.cgi';
     var data = {
@@ -13,5 +14,30 @@ function getAppointments() {
         ajax_query: '123',
     };
     console.log(data);
-    return false;
+
+    $.ajax({
+        async: true,
+        url: url,
+        data: data,
+        type: 'POST',
+        //contentType: "application/json; charset=utf-8",
+        timeout: 4000,
+        success: function(data) {
+            console.log(data);
+
+            //$.each(data, function(i, item) {
+            //    var markup = "<tr><td>" + item.appointment_time + "</td><td>" + item.appointment_description + "</td></tr>";
+            //    $("#existing_appointments tbody").append(markup);
+            //});
+
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) {
+            console.log(XMLHttpRequest);
+            response = XMLHttpRequest.responseJSON.error.Message;
+            console.log(response);
+        }
+    });
+
+
+    return true;
 };
